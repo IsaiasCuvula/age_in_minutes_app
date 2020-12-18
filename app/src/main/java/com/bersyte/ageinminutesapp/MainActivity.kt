fc43a9456faacf475b585738c5d9602f489c26fc
Isaias_Bernardo <bersyteinfo@gmail.com>
2020-12-18 21:07:40 +0200

    second commit

--- app/src/main/java/com/bersyte/ageinminutesapp/MainActivity.kt
+++ app/src/main/java/com/bersyte/ageinminutesapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.bersyte.ageinminutesapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.bersyte.ageinminutesapp.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +16,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSelectDate.setOnClickListener {view->
+            clickDatePicker(view)
+        }
+
+    }
+
+    private fun clickDatePicker(view: View){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+       val dpd = DatePickerDialog(this, {
+                    view, year, month, dayOfMonth ->
+
+            val selectedDate = "$dayOfMonth/${month + 1}/$year"
+
+            binding.tvSelectedDate.text = selectedDate
+
+            val sdf =  SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            val theDate = sdf.parse(selectedDate)
+
+            val selectedDateInMinute = theDate!!.time / 60000
+
+            val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+            val currentDateToMinute = currentDate!!.time / 60000
+
+            val differenceInMinutes = currentDateToMinute - selectedDateInMinute
+            binding.tvSelectedDateInMinute.text = differenceInMinutes.toString()
+
+            }, year, month, day)
+
+        dpd.datePicker.maxDate = Date().time - 86400000
+        dpd.show()
+
     }
 }
